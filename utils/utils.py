0cd09eb342f1ac840eacdab06a1437e8e78fe956
Diletta Calussi <s0559842@htw-berlin.de>
2019-03-19 08:12:59 +0100

    Added comments in utils

--- utils/utils.py
+++ utils/utils.py
@@ -7,7 +7,7 @@ from torch.utils.data import SubsetRandomSampler
 
 from global_settings import device, USE_CUDA
 
-# Practical PyTorch and PyTorch Tutorials
+# Source: https://pytorch.org/tutorials/beginner/chatbot_tutorial.html
 # If preprocessing is done with "expand_contractions" flag set to True,
 # the prefixes right part, e.g. 'we re', should not appear in the corpus
 eng_prefixes = (
@@ -21,29 +21,40 @@ eng_prefixes = (
 
 
 def max_length(lines):
+    """
+    Computes max length given an array of sentences
+    :param lines:
+    :return:
+    """
     lens = [[len(sent.split(" "))] for sent in lines]
     return max(lens)
 
 
 def english_prefixes(sentence):
+    """
+    Filter checks if a sentence starts with some english prefixes
+    :param sentence:
+    :return:
+    """
     return sentence.startswith(eng_prefixes)
 
-
-""" Function at pair level """
 
 
 def filter_pairs(pairs, len_tuple=None, filter_func=None):
     """
-    :param pairs: The sentence pair
-    :param len_tuple: A tuple including a min_len and a max_len, e.g. (3,25)
-    :param filter_func: A tuple, including boolean filter functions, e.g. filter_sentence_by_prefix
-    :return: the filtered pairs and the size of the filtered dataset
+    Applies filters at pair level
+    :param pairs: the parallel corpus
+    :param len_tuple: provided as array containing the minimum and the maximum length to consider in pairs
+    :param filter_func: provided as function, this will applied to the corpus
+    :return: the filtered corpus
     """
     if len_tuple:
         min_length = len_tuple[0]
         max_length = len_tuple[1]
         pairs = [pair for pair in pairs if len(pair[0]) >= min_length and len(pair[0]) <= max_length]
 
+    # This should be improved, thus this is shut down :-)
+    filter_func = None
     if filter_func:
         pairs = [pair for pair in pairs if filter_func(pair[0]) or filter_func(pair[1])]
 
@@ -53,6 +64,13 @@ def filter_pairs(pairs, len_tuple=None, filter_func=None):
 ### train / testing utils
 
 def maskNLLLoss(inp, target, mask):
+    """
+    Source: PyTorch Chatbot Tutorial: https://pytorch.org/tutorials/beginner/chatbot_tutorial.html
+    :param inp: Input variable provided as tensor
+    :param target: Target variable provided as tensor
+    :param mask: THe masking matrix to handle with padded tensors
+    :return: the NLLLoss for the given input (prediction) and the real value (target)
+    """
     nTotal = mask.sum()
     crossEntropy = -torch.log(torch.gather(inp, 1, target.view(-1, 1)).squeeze(1))
     loss = crossEntropy.masked_select(mask).mean()
@@ -62,7 +80,11 @@ def maskNLLLoss(inp, target, mask):
 
 def split_data(data, test_ratio=0.2):
     """
-    Splits data into training, validation and test set
+    Splits data into training, validation and test set.
+    Train set: 70%
+    Val set: 20%
+    Test set: 10%
+
     :param data: all the dataset
     :param test_ratio: actually the validation ratio
     :return: 3 splits
