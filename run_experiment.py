7eeac5b0af4c5701a9b8b31a6bb37ca704016762
Diletta Calussi <s0559842@htw-berlin.de>
2019-03-25 19:15:32 +0100

    test set not random

--- run_experiment.py
+++ run_experiment.py
@@ -211,9 +211,11 @@ if __name__ == '__main__':
         print("Source vocabulary:", input_lang.num_words)
         print("Target vocabulary:", output_lang.num_words)
 
+    test_batches = [batch2TrainData(input_lang, output_lang, pair) for pair in test_set]
 
-    test_batches = [batch2TrainData(input_lang, output_lang, [random.choice(test_set) for _ in range(1)])
-                        for _ in range(len(test_set))]
+    print(len(test_batches))
+
+    exit()
 
     print("Test batches:", len(test_batches))
 
